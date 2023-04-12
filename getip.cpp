065723d47bc312ab1273622845f5506241694849
DESKTOP-QQ9UPR1\vdrive <dengyintao@foxmail.com>
2023-04-12 17:56:57 +0800

    添加ip监控通知逻辑

--- getip.cpp
+++ getip.cpp
@@ -1,39 +1,78 @@
 #include <iostream>
+#include <unistd.h>
+#include <string>
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <arpa/inet.h>
-#include <string>
-#include <boost/timer.hpp>
-#define ETH_NAME    "wlan0"  //如果要获取其他网卡的地址，将这个换为其他网卡名称，比如eth0
+#include <boost/version.hpp>
+#include <boost/config.hpp>
+#include <boost/format.hpp>
+#include "httplib.h"
+#define ETH_NAME "wlan0"  //如果要获取其他网卡的地址，将这个换为其他网卡名称，比如eth0
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+	
+std::string get_ip();
+void get_mac(char * mac_a);
+bool PushMessagetoIOS(std::string msg);
 			     //
-	std::string get_ip();
-	void get_mac(char * mac_a);
-			     //
-	int main()
-	{
-	    //ip
-            boost::timer t;
-	    std::cout<<"maxtime span:"<<t.elapsed_max()/3600<<std::endl;
-	    std::string ip = get_ip();
-	    printf("current_ip: %s\n", ip.c_str());
-	    return 0;
-	 }
+int main()
+{
+	 std::string ip = "";
+    //监控ip变化
+ while(1)
+ {
+ 	std::string iptemp = get_ip();
+ 	if (ip!=iptemp)
+ 	{
+ 		boost::format fmt("当前ip地址:%s");
+ 		fmt%iptemp;
+ 		std::string msg = fmt.str();
+ 		bool bSend = false;
+ 		while (!bSend)
+ 		{
+ 			bSend = PushMessagetoIOS(msg);
+ 			if (!bsend)
+ 			{
+ 				printf("current_ip: %s 发送失败\n", iptemp.c_str());
+ 			}	
+ 		}
+ 	}		
+ 	sleep(10); 
+ }
+	return 0;
+}
         
-         std::string get_ip()
-	{
-	  int                 sockfd;
-	  struct sockaddr_in  sin;
-	  struct ifreq        ifr;
-	  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	  if (sockfd == -1) 
-	  {
-	        perror("socket error");
-	        exit(1);
-	  }
-	  strncpy(ifr.ifr_name, ETH_NAME, IFNAMSIZ);      //Interface name
-	  if (ioctl(sockfd, SIOCGIFADDR, &ifr) == 0) 
-	  {    //SIOCGIFADDR 获取interface address
-	       memcpy(&sin, &ifr.ifr_addr, sizeof(ifr.ifr_addr));
-	       return inet_ntoa(sin.sin_addr);
-	  }
-	 }
+std::string get_ip()
+{
+  int                 sockfd;
+  struct sockaddr_in  sin;
+  struct ifreq        ifr;
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd == -1) 
+  {
+     return "127.0.0.1"
+  }
+
+  strncpy(ifr.ifr_name, ETH_NAME, IFNAMSIZ);      //Interface name
+  if (ioctl(sockfd, SIOCGIFADDR, &ifr) == 0) 
+  {   //SIOCGIFADDR 获取interface address
+     memcpy(&sin, &ifr.ifr_addr, sizeof(ifr.ifr_addr));
+     return inet_ntoa(sin.sin_addr);
+  }
+ }
+
+ bool PushMessagetoIOS(std::string msg)
+ {
+	bool bRet = false;
+	std::string url;
+	boost::format fmt("/g8HF6uCjc2QrWDUiPthnPf/%s/%s");
+	fmt%"termux"%msg;
+	httplib::Client cli("http://api.day.app");
+	std::string info = fmt.str();
+	auto res = cli.Get(info.c_str());
+	if(res->status)
+		bRet = true;
+	else
+		bRet = false;
+  return bRet;
+ }
